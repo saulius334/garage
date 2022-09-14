bbed7d73391b699c075465d288ad7826652e196f
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-14 13:10:29 +0300

    seeder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MechanicController as mechCon;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('mechanic')->name('m_')->group(function () {
+    Route::get('/', [mechCon::class, 'index'])->name('index');
+    Route::get('/create', [mechCon::class, 'create'])->name('create');
+    Route::post('/create', [mechCon::class, 'store'])->name('store');
+    Route::get('/show/{mechanic}', [mechCon::class, 'show'])->name('show');
+    Route::delete('/delete/{mechanic}', [mechCon::class, 'destroy'])->name('delete');
+    Route::get('/edit/{mechanic}', [mechCon::class, 'edit'])->name('edit');
+    Route::put('/edit/{mechanic}', [mechCon::class, 'update'])->name('update');
+});
