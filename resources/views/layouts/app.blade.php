fc99172dc2f2c8b6750bf2b0e85b7fb0bc210fa1
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-15 14:09:42 +0300

    app > msg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -90,43 +90,7 @@
             </div>
         </nav>
         <main class="py-4">
-
-            @if(Session::has('success_msg'))
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-5">
-                        <div class="alert alert-success alert-dismissible fade show">
-                            {{ Session::get('success_msg')  }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            @endif
-            @if(Session::has('danger_msg'))
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-5">
-                        <div class="alert alert-danger alert-dismissible fade show">
-                            {{ Session::get('danger_msg')  }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            @endif
-            @if(Session::has('info_msg'))
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-5">
-                        <div class="alert alert-info alert-dismissible fade show">
-                            {{ Session::get('info_msg')  }}
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            @endif
+           @include('layouts.msg')
             @yield('content')
         </main>
     </div>
