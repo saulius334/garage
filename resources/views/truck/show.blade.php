24bc9ec638ee46801cf54672fc0f353d8ae6a69e
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-15 14:30:02 +0300

    col9

--- resources/views/truck/show.blade.php
+++ resources/views/truck/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-5">
+        <div class="col-9">
             <div class="card">
                 <div class="card-header">
                     <h2>Truck</h2>
