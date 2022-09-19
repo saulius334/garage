da9ab5169f934db4ee03d09207c87aa49aaec930
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-19 14:25:10 +0300

    delete fix + intervention

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -111,6 +111,22 @@ class TruckController extends Controller
         $truck ->make_year = $request->make_year;
         $truck ->mechanic_notices = $request->mechanic_notices;
         $truck ->mechanic_id = $request->mechanic_id;
+        if ($request->delete_photo) {
+            unlink(public_path().'/trucks/' .pathinfo($truck->photo, PATHINFO_FILENAME).'.'.pathinfo($truck->photo, PATHINFO_EXTENSION));
+            $truck->photo = null;
+        }
+
+        if ($request->file('photo')) {
+            if ($truck->photo) {
+                unlink(public_path().'/trucks/' .pathinfo($truck->photo, PATHINFO_FILENAME).'.'.pathinfo($truck->photo, PATHINFO_EXTENSION));
+            }
+            $photo = $request->file('photo');
+            $ext = $photo->getClientOriginalExtension();
+            $name = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+            $file = $name. '-' . rand(100000, 999999). '.' . $ext;
+            $photo->move(public_path().'/trucks', $file);
+            $truck->photo = asset('/trucks') . '/' . $file;
+        }
         $truck->save();
         return redirect()->route('t_index');
     }
--- resources/views/truck/edit.blade.php
+++ resources/views/truck/edit.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-6">
+        <div class="col-8">
             <div class="card">
                 <div class="card-header">
                     <h2>Edit Truck</h2>
                 </div>
                 <div class="card-body">
-                    <form action="{{route('t_update', $truck)}}" method="post">
+                    <form action="{{route('t_update', $truck)}}" method="post" enctype="multipart/form-data">
                     <div class="input-group mb-3">
                         <span class="input-group-text">Maker</span>
                         <input value="{{old('maker', $truck->maker)}}" type="text" name="maker" class="form-control">
@@ -25,6 +25,21 @@
                         <span class="input-group-text">Notices</span>
                         <textarea name="mechanic_notices" class="form-control">{{old('mechanic_notices', $truck->mechanic_notices)}}</textarea>
                     </div>
+                    @if($truck->photo)
+                        <div class="img-small mt-3">
+                            <img src="{{$truck->photo}}">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="1" id="del-photo" name="delete_photo">
+                                <label class="form-check-label" for="del-photo">
+                                    Delete photo
+                                </label>
+                            </div>
+                        </div>
+                        @endif
+                        <div class="input-group mt-3">
+                            <span class="input-group-text">Photo</span>
+                            <input type="file" name="photo" class="form-control">
+                        </div>
                     <select name="mechanic_id" class="form-select mt-3">
                         <option value="0">Choose mechanic</option>
                         @foreach ($mechanics as $value)
