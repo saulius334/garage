a8ab969e1b40e1a10fbc10805e14cadb8d29f8e0
saulius334 <105633620+saulius334@users.noreply.github.com>
2022-09-26 14:36:05 +0300

    modal fix close

--- resources/views/breakdown/modal_content.blade.php
+++ resources/views/breakdown/modal_content.blade.php
@@ -1,46 +1,48 @@
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="edit-modal-label">Edit breakdown</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <div class="card">
-              <div class="card-body">
-                <h2>{{$breakdown->getTruck->getMechanic->name}} {{$breakdown->getTruck->getMechanic->surname}}</h2>
-                <h2>{{$breakdown->getTruck->plate}}</h2>
-                    <div id="trucksList"></div>
-                      <div class="input-group mt-3">
-                          <span class="input-group-text">Title</span>
-                          <input value="{{$breakdown->title}}" data-create type="text" name="title" class="form-control">
-                      </div>
+<div class="modal-content">
+  <div class="modal-header">
+      <h5 class="modal-title" id="edit-modal-label">Edit breakdown</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body">
+      <div class="card">
+          <div class="card-body">
 
-                      <div class="input-group mt-3">
-                          <span class="input-group-text">Notes</span>
-                          <textarea data-create name="notes" class="form-control">{{$breakdown->notes}}</textarea>
-                      </div>
+              <h2>{{$breakdown->getTruck->getMechanic->name}} {{$breakdown->getTruck->getMechanic->surname}}</h2>
+              <h2>{{$breakdown->getTruck->plate}}</h2>
 
-                      <select data-create name="status" class="form-select mt-3">
-                          @foreach ($status as $key => $value)
-                          <option value="{{$key}}" @if($key == $breakdown->status) selected @endif>{{$value}}</option>
-                          @endforeach
-                        </select>
+              <div id="trucks-list"></div>
 
-                    <div class="input-group mt-3">
-                      <span class="input-group-text">Price</span>
-                      <input value="{{$breakdown->price}}" data-create type="text" name="price" class="form-control">
-                  </div>
-
-                  <div class="input-group mt-3">
-                      <span class="input-group-text">Discount</span>
-                      <input value="{{$breakdown->discount}}" data-create type="text" name="discount" class="form-control">
-                  </div>
+              <div class="input-group mb-3">
+                  <span class="input-group-text">Title</span>
+                  <input data-create type="text" name="title" class="form-control" value="{{$breakdown->title}}">
               </div>
+
+              <div class="input-group mb-3">
+                  <span class="input-group-text">Notes</span>
+                  <textarea data-create class="form-control" name="notes">{{$breakdown->notes}}</textarea>
+              </div>
+
+              <select data-create name="status" class="form-select mb-3">
+                  @foreach($status as $key => $value)
+                  <option value="{{$key}}" @if($key == $breakdown->status) selected @endif>{{$value}}</option>
+                  @endforeach
+              </select>
+
+              <div class="input-group mb-3">
+                  <span class="input-group-text">Price</span>
+                  <input data-create type="text" name="price" class="form-control" value="{{$breakdown->price}}">
+              </div>
+
+              <div class="input-group mb-3">
+                  <span class="input-group-text">Discount</span>
+                  <input data-create type="text" name="discount" class="form-control" value="{{$breakdown->discount}}">
+              </div>
+
           </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
+  </div>
+  <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+  </div>
+</div>
